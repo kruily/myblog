b64968a008821e5c9161370c95c1ea3d6f606ad7
jinxiu1016 <1693186883@qq.com>
2020-07-17 17:08:36 +0800

    '2020-7-17 17:08:27'

--- link/views.py
+++ link/views.py
@@ -11,10 +11,10 @@ def link(request):
         return render(request, 'link/list.html',context=context)
 
 def add_link(request):
-    link = Link.objects.create()
     if request.method == 'POST':
         link_form = LinkForm(request.POST,request.FILES)
         if link_form.is_valid():
+            link = link_form.save(commit=False)
             link.name = link_form.cleaned_data['name']
             link.url = link_form.cleaned_data['url']
             if 'avatar' in request.FILES:
@@ -22,7 +22,9 @@ def add_link(request):
             link.save()
             return redirect('links:link_form')
         else:
-            return HttpResponse("表单信息有误!")
+            links = Link.objects.all()
+            context = {'links':links,'message':"友友,表单信息有误!"}
+            return render(request,'link/list.html',context=context)
     else:
-        return HttpResponse("仅支持POST访问");
+        return HttpResponse("仅支持POST访问")
 
